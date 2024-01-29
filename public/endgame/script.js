151e49eaaa0c55bf7fd62c35d941b305746c0389
AlexanderGardiner <misc@alexandergardiner.com>
2024-01-28 20:38:52 -0800

    Endgame now gets stored too

--- public/endgame/script.js
+++ public/endgame/script.js
@@ -1,4 +1,8 @@
 function nextButton() {
+  localStorage.setItem("Park", document.getElementById("Park").value);
+  localStorage.setItem("Onstage", document.getElementById("Onstage").value);
+  localStorage.setItem("Spotlit", document.getElementById("Spotlit").value);
+  localStorage.setItem("Harmony", document.getElementById("Harmony").value);
   window.location.href = "../submit";
 }
 
